fc0ee4ecafda51e152c38876b17b936a7ef5f8a1
Özalp Döndüren <ozalp@donduren.com>
2015-11-03 01:57:11 +0200

    SizeRates on PO Create view

--- Source/CriticalPath.Web/App_Start/BundleConfig.cs
+++ Source/CriticalPath.Web/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace CriticalPath.Web
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui-{version}.js",
+                        "~/Scripts/utilities.jq.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ Source/CriticalPath.Web/Scripts/utilities.jq.js
@@ -0,0 +1,9 @@
+﻿function numbersOnly(evt) {
+    var arrayExceptions = [8, 9, 13, 16, 17, 18, 20, 27, 35, 36, 37,
+        38, 39, 40, 45, 46, 144];
+    if ((evt.keyCode < 48 || evt.keyCode > 57) &&
+            (evt.keyCode < 96 || evt.keyCode > 106) && // NUMPAD
+            $.inArray(evt.keyCode, arrayExceptions) === -1) {
+        return false;
+    }
+}
